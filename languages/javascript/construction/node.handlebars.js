1186296752a0a63cd8e6c6308550eaa11627597c
Maximilian Klein <dasmaxl@gmail.com>
2014-06-25 23:39:03 +0200

    redesigning semantic/implementation definitions

--- languages/javascript/construction/node.handlebars.js
+++
@@ -1,9 +0,0 @@
-function Node_{{generic.id}} (InQueues, OutQueues){
-{{#ifCond node.atomic node.implemented}}\
-  var name = "{{generic.id}}";
-  var meta = {{meta}};
-  csp.go({{generic.name}},[InQueues, OutQueues, name, meta]);
-{{else}}\
-  Group_{{generic.id}}(InQueues, OutQueues);
-{{/ifCond}}
-}
