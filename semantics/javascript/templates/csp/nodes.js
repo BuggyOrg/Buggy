1bfbb9bf3153598a956a79f649c543bf30db0213
Maximilian Klein <mklein@welfenlab.de>
2014-07-09 00:21:51 +0200

    implementation and node specific impls

--- semantics/javascript/templates/csp/nodes.js
+++ semantics/javascript/templates/csp/nodes.js
@@ -1,6 +1,6 @@
 function Node_{{node.id}} (InQueues, OutQueues, meta){
 {{#if implementation.atomic}}
   var name = "{{node.id}}__{{node.mangle}}";
-  csp.go({{implementation.name}},[InQueues, OutQueues, name, meta]);
+  csp.go({{implementation.name}}_{{node.id}},[InQueues, OutQueues, name, meta]);
 {{/if}}
 }
