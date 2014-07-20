5a1e78008cbd27698a550590f36d95f5294cf8c0
Maximilian Klein <dasmaxl@gmail.com>
2014-07-20 15:18:05 +0200

    semantics

--- source-refs.js
+++ source-refs.js
@@ -1,7 +1,47 @@
 sourceRefs = {
+  "Semantic Field/Construction": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/construction.ls",
+    "line": "21",
+    "ref": "Semantic Field/Construction"
+  },
+  "Semantic Field/Construction/Hooks": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/construction.ls",
+    "line": "19",
+    "ref": "Semantic Field/Construction/Hooks"
+  },
+  "Semantic Field/Implementations/Hooks": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/implementation.ls",
+    "line": "20",
+    "ref": "Semantic Field/Implementations/Hooks"
+  },
+  "Semantic Field/Implementations": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/implementation.ls",
+    "line": "22",
+    "ref": "Semantic Field/Implementations"
+  },
+  "Semantic Field/Construction/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/construction.ls",
+    "line": "19",
+    "ref": "Semantic Field/Construction/Predicates"
+  },
+  "Semantic Field/Implementations/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/implementation.ls",
+    "line": "19",
+    "ref": "Semantic Field/Implementations/Predicates"
+  },
+  "Semantic Field/Symbols/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/symbols.ls",
+    "line": "19",
+    "ref": "Semantic Field/Symbols/Predicates"
+  },
   "Semantic Field": {
     "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/semantic-field.ls",
     "line": "20",
     "ref": "Semantic Field"
+  },
+  "Semantic Field/Symbols": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/symbols.ls",
+    "line": "20",
+    "ref": "Semantic Field/Symbols"
   }
 }
