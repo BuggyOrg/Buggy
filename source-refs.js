9e6dff7807078dbce07e683b1928879008075aa7
Maximilian Klein <dasmaxl@gmail.com>
2014-08-16 20:53:08 +0200

    api includes all functions but needs descriptions

--- source-refs.js
+++ source-refs.js
@@ -1,1 +1,97 @@
-sourceRefs = {}
+sourceRefs = {
+  "Semantic Field/Construction": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/construction.ls",
+    "line": "21",
+    "ref": "Semantic Field/Construction"
+  },
+  "Semantic Field/Construction/Hooks": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/construction.ls",
+    "line": "19",
+    "ref": "Semantic Field/Construction/Hooks"
+  },
+  "Semantic Field/Implementations/Nodes": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/implementation-connections.ls",
+    "line": "20",
+    "ref": "Semantic Field/Implementations/Nodes"
+  },
+  "Semantic Field/Implementations/Hooks": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/implementation.ls",
+    "line": "20",
+    "ref": "Semantic Field/Implementations/Hooks"
+  },
+  "Semantic Field/Implementations/Atomics": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/implementation.ls",
+    "line": "23",
+    "ref": "Semantic Field/Implementations/Atomics"
+  },
+  "Semantic Field/Implementations/NodesRef": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/implementation.ls",
+    "line": "42",
+    "ref": "Semantic Field/Implementations/NodesRef"
+  },
+  "Semantic Field/Modules/Hooks": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/hooks/modules.ls",
+    "line": "19",
+    "ref": "Semantic Field/Modules/Hooks"
+  },
+  "Semantic Field/Implementations": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/implementation.ls",
+    "line": "22",
+    "ref": "Semantic Field/Implementations"
+  },
+  "TODO: Better loading mechanism": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/loading.ls",
+    "line": "28",
+    "ref": "TODO: Better loading mechanism"
+  },
+  "Semantic Field/Meta": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/meta.ls",
+    "line": "21",
+    "ref": "Semantic Field/Meta"
+  },
+  "Semantic Field/Modules": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/modules.ls",
+    "line": "21",
+    "ref": "Semantic Field/Modules"
+  },
+  "Semantic Field/Construction/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/construction.ls",
+    "line": "19",
+    "ref": "Semantic Field/Construction/Predicates"
+  },
+  "Semantic Field/Implementations/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/implementation.ls",
+    "line": "19",
+    "ref": "Semantic Field/Implementations/Predicates"
+  },
+  "Semantic Field/Meta/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/meta.ls",
+    "line": "19",
+    "ref": "Semantic Field/Meta/Predicates"
+  },
+  "Semantic Field/Modules/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/modules.ls",
+    "line": "18",
+    "ref": "Semantic Field/Modules/Predicates"
+  },
+  "Semantic Field/Symbols/Predicates": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/predicates/symbols.ls",
+    "line": "19",
+    "ref": "Semantic Field/Symbols/Predicates"
+  },
+  "Semantic Field": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/semantic-field.ls",
+    "line": "20",
+    "ref": "Semantic Field"
+  },
+  "Semantic Field/Symbols": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/src/semantics/symbols.ls",
+    "line": "20",
+    "ref": "Semantic Field/Symbols"
+  },
+  "MathParser Module": {
+    "link": "https://github.com/BuggyOrg/Buggy/blob/develop/semantics/modules/processing/parse-math.ls",
+    "line": "17",
+    "ref": "MathParser Module"
+  }
+}
